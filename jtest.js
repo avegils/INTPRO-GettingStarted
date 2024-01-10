49e6faf9f939723e53571cc49bf7da774a0f8f31
avegils <114716886+avegils@users.noreply.github.com>
2024-01-10 12:52:25 -0500

    Added initial files for checkup

---
+++ jtest.js
@@ -0,0 +1,16 @@
+/*
+    TITLE: Getting Started
+    AUTHOR: Avery A. Gilstrap
+    CREATE DATE: 9 Jan 2024
+    PURPOSE: Understand the basics of HTML, CSS, and JS and Uploading Projects
+    LAST MODIFIED ON: 10 Jan 2024
+    LAST MODIFIED BY: Avery A. Gilstrap
+    MODIFICATION HISTORY:
+    10 Jan 2024 - Created base JS file; Added content for JS to write to main page.
+*/
+
+document.write('<h3>Find my voice.</h3>');
+document.write('<h2>Although, it sounds like bits and bytes</h2>');
+document.write('<h3>My circuitry is filled with mites</p>');
+document.write('<p>Hello, World.</p>');
+// This will finish the lyrics on the main page, by writing the final four lines and stylying them with CSS
